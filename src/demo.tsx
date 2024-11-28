4d44c76534a85c66a9fb8a7033bd59928f956317
Zora Péteri <zorapeteri@gmail.com>
2024-11-28 19:33:54 +0100

    docs initial

--- src/demo.tsx
+++
@@ -1,9 +0,0 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import MyComponent from './main';
-
-// ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
-//   <React.StrictMode>
-//     <MyComponent />
-//   </React.StrictMode>
-// );
