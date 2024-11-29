c62fc0eebcedae6fddc6814a90f27a353a4cbdec
Zora Péteri <zorapeteri@gmail.com>
2024-11-29 17:57:22 +0100

    add demo component

--- src/demo.tsx
+++ src/demo.tsx
@@ -1,9 +1,30 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom/client';
-// import MyComponent from './main';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ThreadsCard from './main';
 
-// ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
-//   <React.StrictMode>
-//     <MyComponent />
-//   </React.StrictMode>
-// );
+ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
+  <React.StrictMode>
+    <ThreadsCard
+      style={{ width: '100%', maxWidth: '500px', margin: '100px auto' }}
+      author={{
+        username: 'samreich',
+        image:
+          'https://zorapeteri.github.io/react-threads-card/img/samreich.jpg',
+        verified: true,
+      }}
+      text="that's a wrap on game changer season 7"
+      images={[
+        {
+          src: 'https://zorapeteri.github.io/react-threads-card/img/season7.jpg',
+        },
+        {
+          src: 'https://zorapeteri.github.io/react-threads-card/img/season7.jpg',
+        },
+        {
+          src: 'https://zorapeteri.github.io/react-threads-card/img/season7.jpg',
+        },
+      ]}
+      time={new Date('2024-11-14T17:01:00Z')}
+    />
+  </React.StrictMode>
+);
